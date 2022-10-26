237738fea70f5a9169e216b022c8221f1bd92bd9
sourabh.2021ca104@mnnit.ac.in <sourabh.2021ca104@mnnit.ac.in>
2022-10-26 17:42:15 +0530

    adding profile , explore , create community activity

--- ConnectApp/app/src/main/java/com/callback/connectapp/fragment/CommunityFragment.java
+++ ConnectApp/app/src/main/java/com/callback/connectapp/fragment/CommunityFragment.java
@@ -1,19 +1,24 @@
 package com.callback.connectapp.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.callback.connectapp.Activity.LoginActivity;
+import com.callback.connectapp.Activity.createCommunity;
+import com.callback.connectapp.Activity.exploreCommunity;
 import com.callback.connectapp.R;
 
 
 public class CommunityFragment extends Fragment {
 
-
+     CardView exploreCommunity,createCommunity;
 
     public CommunityFragment () {
         // Required empty public constructor
@@ -26,6 +31,38 @@ public class CommunityFragment extends Fragment {
     public View onCreateView (LayoutInflater inflater , ViewGroup container ,
                               Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_community , container , false);
+
+
+
+
+        View view= inflater.inflate(R.layout.fragment_community , container , false);
+
+        exploreCommunity =view.findViewById(R.id.exploreCommunity);
+        createCommunity =view.findViewById(R.id.createCommunity);
+
+
+
+        exploreCommunity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View view) {
+
+                Intent i= new Intent(getContext(), exploreCommunity.class);
+
+                startActivity(i);
+            }
+        });
+
+        createCommunity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View view) {
+
+                Intent i= new Intent(getContext(), createCommunity.class);
+
+                startActivity(i);
+            }
+        });
+
+
+        return view;
     }
 }
